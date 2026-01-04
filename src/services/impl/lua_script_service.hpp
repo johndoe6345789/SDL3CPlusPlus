1da2fd757a133204a43d64e163cf4ffe310e3f61
johndoe6345789 <john.doe6345789@gmail.com>
2026-01-04 14:47:59 +0000

    feat: Add ILogger dependency to LuaScriptService for improved logging

--- src/services/impl/lua_script_service.hpp
+++ src/services/impl/lua_script_service.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../interfaces/i_script_service.hpp"
+#include "../interfaces/i_logger.hpp"
 #include "../../script/script_engine.hpp"
 #include "../../di/lifecycle.hpp"
 #include <memory>
@@ -18,7 +19,7 @@ class LuaScriptService : public IScriptService,
                          public di::IInitializable,
                          public di::IShutdownable {
 public:
-    explicit LuaScriptService(const std::filesystem::path& scriptPath, bool debugEnabled = false);
+    explicit LuaScriptService(const std::filesystem::path& scriptPath, std::shared_ptr<ILogger> logger, bool debugEnabled = false);
     ~LuaScriptService() override;
 
     // Lifecycle
@@ -44,6 +45,7 @@ public:
     std::string GetLuaError() override;
 
 private:
+    std::shared_ptr<ILogger> logger_;
     std::unique_ptr<script::ScriptEngine> engine_;
     std::filesystem::path scriptPath_;
     bool debugEnabled_ = false;
