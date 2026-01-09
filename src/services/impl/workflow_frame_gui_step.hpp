9bb4b22f67425f30f16422f0fcce71128d625c8d
johndoe6345789 <john.doe6345789@gmail.com>
2026-01-09 21:55:21 +0000

    ROADMAP.md

---
+++ src/services/impl/workflow_frame_gui_step.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../interfaces/i_workflow_step.hpp"
+#include "../interfaces/i_logger.hpp"
+#include "../interfaces/i_input_service.hpp"
+
+#include <memory>
+
+namespace sdl3cpp::services::impl {
+
+class WorkflowFrameGuiStep final : public IWorkflowStep {
+public:
+    WorkflowFrameGuiStep(std::shared_ptr<IInputService> inputService,
+                         std::shared_ptr<ILogger> logger);
+
+    std::string GetPluginId() const override;
+    void Execute(const WorkflowStepDefinition& step, WorkflowContext& context) override;
+
+private:
+    std::shared_ptr<IInputService> inputService_;
+    std::shared_ptr<ILogger> logger_;
+};
+
+}  // namespace sdl3cpp::services::impl
